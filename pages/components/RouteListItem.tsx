88088624b45fbd3f55acb448318e0813e3bfa5f3
Dev Patel <devarshi.ap@gmail.com>
2022-08-22 23:41:06 -0400

    add: req+res example code blocks

--- pages/components/RouteListItem.tsx
+++ pages/components/RouteListItem.tsx
@@ -1,16 +1,46 @@
+import { Prism } from '@mantine/prism';
+import { useState } from 'react';
+
 interface RouteItemProps {
     endpoint: string,
     endpointText: string,
     verbs: string[],
+    serverJSON: any,
 }
 
-export default function RouteListItem({ endpoint, endpointText, verbs}: RouteItemProps) {
+export default function RouteListItem({ endpoint, endpointText, verbs, serverJSON}: RouteItemProps) {
     
     const baseURL: string = "https://skool-rest-api.herokuapp.com/api";
+    const [showReq, setShowReq] = useState(false);
+    const [showRes, setShowRes] = useState(false);
+
+    const toggleShowReq = () => {
+        setShowReq(!showReq);
+    }
+
+    const toggleShowRes = () => {
+        setShowRes(!showRes);
+    }
+
     let verbsList: any;
-    if (verbs) {
+    if (verbs && serverJSON) {
         verbsList = verbs.map((verb: string, index: number) => (
-            <li key={index}><code>{verb.split("-")[0]}</code> - {verb.split("-")[1]}.</li>
+            <>
+                <li key={index}><code>{verb.split("-")[0]}</code> - {verb.split("-")[1]}.</li>
+                <div className="flex flex-col w-full space-y-3 my-8">
+                    <button className='bg-slate-300 text-black p-1' onClick={toggleShowReq}>{showReq ? "▾ Hide" : "‣ Show"} Request : {serverJSON[verb.split("-")[0]].exUrl}</button>
+                    {showReq && (
+                        <Prism language="json" colorScheme="dark">{
+                            JSON.stringify(serverJSON[verb.split("-")[0]]['req'], null, 2).length == 2 ? "No Request Body Required." : JSON.stringify(serverJSON[verb.split("-")[0]]['req'], null, 2)
+                        }</Prism>
+                    )}
+                    
+                    <button className='bg-slate-300 text-black p-1' onClick={toggleShowRes}>{showRes ? "▾ Hide" : "‣ Show"} Response</button>
+                    {showRes && (
+                        <Prism language="json" colorScheme="dark">{JSON.stringify(serverJSON[verb.split("-")[0]]['res'], null, 2)}</Prism>
+                    )}
+                </div>
+            </>
         ))
     } else {
         <li>Something went wrong</li>
